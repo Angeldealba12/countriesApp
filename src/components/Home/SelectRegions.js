9cf2bb0949aae46743f6ac8541dde01cec331c1f
Angel <angel.dealba.garcia@gmail.com>
2021-09-16 20:41:24 -0500

    working on more responsive design

--- src/components/Home/SelectRegions.js
+++ src/components/Home/SelectRegions.js
@@ -16,6 +16,9 @@ const SelectRegions = () => {
 
   return (
     <div className="region-section">
+      <div  style={{marginTop: '2rem'}} className="title-all all2">
+        <h2>Explore by Region</h2>
+      </div>
       <div className="select">
         {regions.map((region) => {
           return (
